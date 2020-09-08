36fc0f4c43bcf2063ab10ac89929ca9d2e8ad281
Denis Timofeev <y719xy@gmail.com>
2020-09-08 18:39:12 +0300

    add configuration Hibernate

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -4,13 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Objects;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
+@Entity
+
 public class User {
+    @Id
     private int id;
     private String username;
     private String password;
